64cfec06c98d334cd0a0a525b61f5b270caa332d
heinrich-foto <github@heinrich-foto.de>
2015-06-16 16:27:05 +0200

    Fenster has the right dimension, but the oldStyle glDrawPixels produce an Bus error:10 if it is the right dimension.

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -1,6 +1,7 @@
 #include <thread>
 #include <renderer.hpp>
 #include <fensterchen.hpp>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
@@ -13,12 +14,12 @@ int main(int argc, char* argv[])
   std::thread thr([&app]() { app.render(); });
 
   Window win(glm::ivec2(width,height));
-
+  std::cout << width << " " << height << std::endl;
   while (!win.shouldClose()) {
     if (win.isKeyPressed(GLFW_KEY_ESCAPE)) {
       win.stop();
     }
-
+    // win.drawLine(0.1f,0.1f, 0.8f,0.1f, 255,0,0);
     glDrawPixels( width, height, GL_RGB, GL_FLOAT
                 , app.colorbuffer().data());
 
