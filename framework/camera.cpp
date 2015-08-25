20e8d5c43e2c5de05afd281449c817a562d7e640
heinrich-foto <github@heinrich-foto.de>
2015-08-25 11:59:21 +0200

    Add Camera Class

---
+++ framework/camera.cpp
@@ -0,0 +1,10 @@
+#include "camera.hpp"
+
+Camera::Camera(std::string name, glm::vec3 pos, float fovX,  float resX) :
+		name_{name}, position_{pos}, fovX_{fovX}, distance_{0} {
+			// todo calculate Distance
+			// distance_= 
+	};
+	
+	// glm::vec3 Camera::translate() {};
+	// glm::vec3 Camera::rotate() {};
---
+++ framework/camera.hpp
@@ -0,0 +1,29 @@
+// -----------------------------------------------------------------------------
+// Copyright  : (C) 2014 Martin Heinrich
+// License    : MIT (see the file LICENSE)
+// Maintainer : Andreas-C. Bernstein <andreas.bernstein@uni-weimar.de>
+// Stability  : experimental
+//
+// Camera
+// -----------------------------------------------------------------------------
+
+#ifndef BUW_CAMERA_HPP
+#define BUW_CAMERA_HPP
+
+#include <glm/vec3.hpp>
+#include <string>
+
+Class camera {
+
+	Camera(std::string name, glm::vec3 pos, float fovX,  float resX);
+	// glm::vec3 translate();
+	// glm::vec3 rotate();
+private:
+	std::string name_;
+	glm::vec3 position_;
+	float fovX_;
+	float distance_;
+	// Translations und Rotationsmatrix??!!!!!
+};
+
+#endif // BUW_CAMERA
