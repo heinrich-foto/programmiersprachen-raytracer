a9f19a8f53c22d436620d7f6e4d8029f42a2ed31
heinrich-foto <github@heinrich-foto.de>
2015-08-25 12:01:35 +0200

    Add Light

---
+++ framework/light.hpp
@@ -0,0 +1,26 @@
+// -----------------------------------------------------------------------------
+// Copyright  : (C) 2014 Martin Heinrich
+// License    : MIT (see the file LICENSE)
+// Maintainer : Andreas-C. Bernstein <andreas.bernstein@uni-weimar.de>
+// Stability  : experimental
+//
+// Light
+// -----------------------------------------------------------------------------
+
+#ifndef BUW_LIGHT_HPP
+#define BUW_LIGHT_HPP
+
+#include <glm/vec3.hpp>
+#include <string>
+
+Class light {
+
+private:
+	std::string name_;
+	glm::vec3 position_;
+	Color la_; // Ambient Light
+	Color ld_; // Diffuse Light
+	Color lp_; // Helligkeit der Punktlichtquelle
+};
+
+#endif // BUW_LIGHT
