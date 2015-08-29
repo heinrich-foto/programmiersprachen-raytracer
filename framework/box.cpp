7756e4cae0d15368f8df3748a17fb648015584f3
Martin Heinrich <github@heinrich-foto.de>
2015-08-29 02:07:46 +0200

    hmmm box liebe box...

--- framework/box.cpp
+++ framework/box.cpp
@@ -31,65 +31,70 @@ glm::vec3 Box::max() const
 	return max_;
 }
 
-// Hit Box::intersect(const Ray &r) const {
-// 	float tmin, tmax, tmin_y, tmax_y, tmin_z, tmax_z;
-
-// 	tmin  = (min_.x - r.origin.x) * r.inv_direction.x;
-// 	tmax  = (max_.x - r.origin.x) * r.inv_direction.x;
-// 	tmin_y = (min_.y - r.origin.y) * r.inv_direction.y;
-// 	tmax_y = (max_.y - r.origin.y) * r.inv_direction.y;
-	
-// 	if ( (tmin > tmax_y) || (tmin_y > tmax) )
-// 	{ 
-// 		//std::cout << tmin <<" > " << tmax_y << " || " <<  tmin_y << " > "  << tmax << std::endl;
-// 		// return Hit {false,tmax, r.origin, shared_from_this()}; 
-// 		return Hit {false,tmax, r.origin, name_};
-// 	}
-// 	if (tmin_y > tmin)
-// 		tmin = tmin_y;
-// 	if (tmax_y < tmax)
-// 		tmax = tmax_y;
-// 	tmin_z = (min_.z - r.origin.z) * r.inv_direction.z;
-// 	tmax_z = (max_.z - r.origin.z) * r.inv_direction.z;
-// 	if ( (tmin > tmax_z) || (tmin_z > tmax) )
-// 	{
-// 		//std::cout << tmin <<" > "<< tmax_z<< " || " <<tmin_z<< " > " << tmax << std::endl;
-// 		// return Hit {false,tmax, r.origin, shared_from_this()};
-// 		return Hit {false,tmax, r.origin, name_};
-// 	}
-// 	if (tmin_z > tmin)
-// 		tmin = tmin_z;
-// 	if (tmax_z < tmax)
-// 		tmax = tmax_z;
-
-// 	std::cout << name_ << " min: " << tmin << " max: " << tmax << r << std::endl;
-//  	// return Hit {true, tmin, r.origin, shared_from_this()};
-//  	return Hit {true, tmin, r.origin, name_};
-// }
 Hit Box::intersect(const Ray &r) const {
- 	float tx1 = (min_.x - r.origin.x)* r.inv_direction.x;
-	float tx2 = (max_.x - r.origin.x)* r.inv_direction.x;
-	float tmin_x = std::min(tx1, tx2);
-	float tmax_x = std::max(tx1, tx2);
+	
+	float tmin_x  = (min_.x - r.origin.x) * r.inv_direction.x;
+	float tmax_x  = (max_.x - r.origin.x) * r.inv_direction.x;
+	if (tmin_x > tmax_x) std::swap(tmin_x,tmax_x);
 
-	float tx3 = (min_.y - r.origin.y)* r.inv_direction.y;
-	float tx4 = (max_.y - r.origin.y)* r.inv_direction.y;
-	float tmin_y = std::min(tx3, tx4);
-	float tmax_y = std::max(tx3, tx4);
+	float tmin_y = (min_.y - r.origin.y) * r.inv_direction.y;
+	float tmax_y = (max_.y - r.origin.y) * r.inv_direction.y;
+	if (tmin_y > tmax_y) std::swap(tmin_y,tmax_x);
+	
+	if ( (tmin_x > tmax_y) || (tmin_y > tmax_x) )
+	{ 
+		//std::cout << tmin <<" > " << tmax_y << " || " <<  tmin_y << " > "  << tmax << std::endl;
+		return Hit {false,tmax_x, r.origin, shared_from_this()}; 
+		// return Hit {false,tmax, r.origin, name_};
+	}
+	if (tmin_y > tmin_x)
+		tmin_x = tmin_y;
+	if (tmax_y < tmax_x)
+		tmax_x = tmax_y;
 
-	float tx5 = (min_.z - r.origin.z)* r.inv_direction.z;
-	float tx6 = (max_.z - r.origin.z)* r.inv_direction.z;
-	float tmin_z = std::min(tx5, tx6);
-	float tmax_z = std::max(tx5, tx6);
+	float tmin_z = (min_.z - r.origin.z) * r.inv_direction.z;
+	float tmax_z = (max_.z - r.origin.z) * r.inv_direction.z;
+	if (tmin_z > tmax_z) std::swap(tmin_z,tmax_z);
 
-	float tmin = std::max(tmin_x, tmin_y);
-	float tmax = std::min(tmax_x, tmax_y);
-	tmin = std::max(tmin, tmin_z);
-	tmax = std::min(tmax, tmax_z);
+	if ( (tmin_x > tmax_z) || (tmin_z > tmax_x) )
+	{
+		//std::cout << tmin <<" > "<< tmax_z<< " || " <<tmin_z<< " > " << tmax << std::endl;
+		return Hit {false,tmax_x, r.origin, shared_from_this()};
+		// return Hit {false,tmax, r.origin, name_};
+	}
+	if (tmin_z > tmin_x)
+		tmin_x = tmin_z;
+	if (tmax_z < tmax_x)
+		tmax_x = tmax_z;
 
-	return Hit {tmax >= tmin, tmax, r.origin, shared_from_this()};
-	// return Hit {tmax >= tmin, tmax, r.origin, name_};
+	// std::cout << name_ << " min: " << tmin << " max: " << tmax << r << std::endl;
+ 	return Hit {true, tmin_x, r.origin, shared_from_this()};
+ 	// return Hit {true, tmin, r.origin, name_};
 }
+// Hit Box::intersect(const Ray &r) const {
+//  	float tx1 = (min_.x - r.origin.x)* r.inv_direction.x;
+// 	float tx2 = (max_.x - r.origin.x)* r.inv_direction.x;
+// 	float tmin_x = std::min(tx1, tx2);
+// 	float tmax_x = std::max(tx1, tx2);
+
+// 	float tx3 = (min_.y - r.origin.y)* r.inv_direction.y;
+// 	float tx4 = (max_.y - r.origin.y)* r.inv_direction.y;
+// 	float tmin_y = std::min(tx3, tx4);
+// 	float tmax_y = std::max(tx3, tx4);
+
+// 	float tx5 = (min_.z - r.origin.z)* r.inv_direction.z;
+// 	float tx6 = (max_.z - r.origin.z)* r.inv_direction.z;
+// 	float tmin_z = std::min(tx5, tx6);
+// 	float tmax_z = std::max(tx5, tx6);
+
+// 	float tmin = std::max(tmin_x, tmin_y);
+// 	float tmax = std::min(tmax_x, tmax_y);
+// 	tmin = std::max(tmin, tmin_z);
+// 	tmax = std::min(tmax, tmax_z);
+
+// 	return Hit {tmax >= tmin, tmax, r.origin, shared_from_this()};
+// 	// return Hit {tmax >= tmin, tmax, r.origin, name_};
+// }
 
 std::ostream& Box::print(std::ostream& os) const
 {
